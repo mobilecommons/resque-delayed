96a8183c635688c1c845d18e128dbdb9d099022c
zioplox11 <joshua.guthals@gmail.com>
2017-10-27 15:58:55 -0400

    Update resque.rb
    pass in a queue name of your choice

--- lib/resque-delayed/resque.rb
+++ lib/resque-delayed/resque.rb
@@ -1,11 +1,11 @@
 module Resque
   class << self
-    def enqueue_at(time, *args)
-      Resque::Delayed.create_at time, *args
+    def enqueue_at(queue, time, *args)
+      Resque::Delayed.create_at(queue, time, *args)
     end
 
-    def enqueue_in(offset, *args)
-      Resque::Delayed.create_in offset, *args
+    def enqueue_in(queue, offset, *args)
+      Resque::Delayed.create_in(queue, offset, *args)
     end
   end
 end
